02650b208bbcf44a9dcb4268b2343ec0bdc945d3
Andrey Kaydalov <sorsarre@gmail.com>
2022-03-27 02:44:10 +0300

    Make it reuse linter happy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+<!--
+SPDX-FileCopyrightText: 2019-2022 Andrey Kaydalov <sorsarre@gmail.com>
+
+SPDX-License-Identifier: BSD-3-Clause
+-->
+
 # hrodvitnir-core
 TBD
--- format-source.sh
+++ format-source.sh
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Andrey Kaydalov <sorsarre@gmail.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 for f in `find . -name '*.*pp'`; do
     echo Formatting $f...
     clang-format -i $f
